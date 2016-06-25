c9af02a26f4beddb8a149d67a5856ec736d87e20
archcorsair <archcorsair@gmail.com>
2016-06-25 00:27:33 -0700

    Cleaned up comments

--- Misc Challenges/queryParse.js
+++ Misc Challenges/queryParse.js
@@ -1,12 +1,9 @@
 // Given a url with query strings,
 // get all query params and values and return an object containg them.
-// http://domain.com/q?=searchstring
 // Not allowed to use Regex
-// make into modudle for node. anyone can require and use it without modification to their code
-// http://domain.com/examplequery?param1=value1&param2=value2&param3=value3
-// reusability is important
+// Make into modudle for node.
 // must be easy to understand (haha oops)
-// handle edge cases - weird shit like unicode.
+// handle unicode
 // example string: google.com/search?value=isearchedthis&othervalue=hello
 // example encoded string: google.com%2Fsearch%3Fvalue1%3Dfoo%26value2%3Dbar%26value3%3Dbaz
 const url = require('url');
