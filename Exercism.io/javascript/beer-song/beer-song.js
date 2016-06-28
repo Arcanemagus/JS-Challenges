a67de0a99b21d4d8ceb7c622575e803d1b6d7a87
archcorsair <archcorsair@gmail.com>
2016-06-27 17:42:26 -0700

    Slight improvements

--- Exercism.io/javascript/beer-song/beer-song.js
+++ Exercism.io/javascript/beer-song/beer-song.js
@@ -3,16 +3,17 @@ class BeerSong {
     const lyrics = {
       bob: 'bottles of beer',
       otw: 'on the wall',
+      twd: 'Take it down and pass it around',
     };
     if (num < 1) {
       return `No more ${lyrics.bob} ${lyrics.otw}, no more ${lyrics.bob}.\n
 Go to the store and buy some more, 99 ${lyrics.bob} ${lyrics.otw}.\n`;
     } else if (num === 1) {
-      return `1 bottle of beer on the wall, 1 bottle of beer.\n
-Take it down and pass it around, no more bottles of beer on the wall.\n`;
+      return `1 bottle of beer ${lyrics.otw}, 1 bottle of beer.\n
+${lyrics.twd}, no more ${lyrics.bob} ${lyrics.otw}.\n`;
     }
     return `${num} ${lyrics.bob} ${lyrics.otw}, ${num} ${lyrics.bob}.\n
-Take one down and pass it around, ${num - 1} ${lyrics.bob} ${lyrics.otw}.\n`;
+${lyrics.twd}, ${num - 1} ${lyrics.bob} ${lyrics.otw}.\n`;
   }
   sing(end, start = 0) {
     const toReturn = [];
@@ -27,5 +28,4 @@ Take one down and pass it around, ${num - 1} ${lyrics.bob} ${lyrics.otw}.\n`;
 }
 const beersong = new BeerSong;
 
-console.log(beersong.sing(8, 6));
-console.log(beersong.verse(12));
+console.log(beersong.sing(22, 9));
