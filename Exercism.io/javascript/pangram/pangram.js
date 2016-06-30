3716ce433e7c338e05042886ebd76c4d48190652
archcorsair <archcorsair@gmail.com>
2016-06-29 18:34:08 -0700

    Begin testing with jasmine-node

--- Exercism.io/javascript/pangram/pangram.js
+++ Exercism.io/javascript/pangram/pangram.js
@@ -1,10 +1,13 @@
 class Pangram {
-  isPangram(str) {
+  constructor(str) {
+    this.str = str;
+  }
+  isPangram() {
     const alphaSet = new Set(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']);
     const newSet = new Set();
-    for (let i = 0, length = str.length; i < length; i++) {
-      const char = str.substr(i, 1);
+    for (let i = 0, length = this.str.length; i < length; i++) {
+      const char = this.str.substr(i, 1);
       if (!alphaSet.has(char)) continue;
       newSet.add(char);
     }
@@ -19,3 +22,6 @@ class Pangram {
 }
 
 module.exports = Pangram;
+
+const pangram = new Pangram('the quick brown fox jumps over the lazy dog');
+console.log(pangram.isPangram());
