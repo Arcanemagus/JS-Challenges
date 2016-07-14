986dbb39d3cb0d36a9ab329a6556feb745df771d
archcorsair <archcorsair@gmail.com>
2016-07-13 20:25:34 -0700

    Passes jasmine spec

--- Exercism.io/javascript/hello-world/hello-world.js
+++ Exercism.io/javascript/hello-world/hello-world.js
@@ -6,34 +6,11 @@
 
 // New ES6 Way
 
-// class HelloWorld {
-//   hello(input) {
-//     return `Hello, ${input || 'World'}`
-//   }
-// }
-// let echo = new HelloWorld;
-// console.log(echo.hello('Meowkins')); // Hello, Meowkins
-
-// Old Pre-ES6 Way
-function HelloWorld() {
-  if (!(this instanceof HelloWorld)) {
-    throw new Error('Cannot call a class as a function');
+class HelloWorld {
+  hello(input) {
+    return `Hello, ${input || 'World'}!`;
   }
 }
-HelloWorld.prototype.hello = function (input) {
-  return `Hello, ${input || 'World'}`;
-};
-
-// const herro = new HelloWorld;
-// console.log(herro.hello('')); // Hello, World
-// console.log(herro.hello()); // Hello, World
-// console.log(herro.hello('James')); // Hello, James
-
+const helloworld = new HelloWorld;
+console.log(helloworld.hello());
 module.exports = HelloWorld;
-
-
-// function hello(input) {
-//   return `Hello, ${input || 'World'}`;
-// }
-// hello(); // Hello, world!
-// hello('James'); // Hello, James!
